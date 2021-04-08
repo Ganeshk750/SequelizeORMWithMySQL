1caf8842820afcd84ec827aaa8e763f8a1b73e66
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-04-08 10:52:32 +0530

    #33

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const _USERs = require('./users.json');
 //const Sequelize = require("sequelize");
 const { Sequelize } = require("sequelize");
 const app = express();
@@ -13,48 +14,73 @@ const connection = new Sequelize("test", "root", "ganesh", {
 });
 
 const User = connection.define("User", {
-  uuid: {
-      type: Sequelize.UUID,
-      primaryKey: true,
-      defaultValue: Sequelize.UUIDV4,
-      allowNull: false
-    },
-   first: Sequelize.STRING,
-   last: Sequelize.STRING,
-   full_name: Sequelize.STRING,
-   bio: Sequelize.TEXT
-  },{
-    hooks:{
-      beforeValidate: () =>{
-        console.log('before Validate');
-      } ,
-      afterValidate: () => {
-        console.log('after validate');
-      },
-      beforeCreate: (user) => {
-        user.full_name = `${user.first} ${user.last}`
-        console.log('before Create');
-      },
-      afterCreate: () => {
-        console.log('after Create');
-      }
+  // uuid: {
+  //     type: Sequelize.UUID,
+  //     primaryKey: true,
+  //     defaultValue: Sequelize.UUIDV4,
+  //     allowNull: false
+  //   },
+  //  first: Sequelize.STRING,
+  //  last: Sequelize.STRING,
+  //  full_name: Sequelize.STRING,
+  //  bio: Sequelize.TEXT
+  // },{
+  //   hooks:{
+  //     beforeValidate: () =>{
+  //       console.log('before Validate');
+  //     } ,
+  //     afterValidate: () => {
+  //       console.log('after validate');
+  //     },
+  //     beforeCreate: (user) => {
+  //       user.full_name = `${user.first} ${user.last}`
+  //       console.log('before Create');
+  //     },
+  //     afterCreate: () => {
+  //       console.log('after Create');
+  //     }
+  //   }
+  // }
+
+
+  //*Adding new Model*//
+
+  name: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: Sequelize.STRING,
+    validate: {
+      isAlphanumeric: true
     }
   }
 
-);
+});
 
 connection
   //   .authenticate()
   .sync({
-    logging: console.log,
+    //logging: console.log,
     force: true,
   })
   .then(() => {
-    User.create({
-      first: "JON",
-      last: "DOE",
-      bio: "This is Test Bio",
-    });
+    // User.create({
+    //   first: "JON",
+    //   last: "DOE",
+    //   bio: "This is Test Bio",
+    // });
+
+    User.bulkCreate(_USERs)
+        .then(users => {
+          console.log('Success adding users');
+        })
+        .catch(error => {
+          console.log(error);
+        })
   })
   .then(() => {
     console.log("Connection has been established successfully.");
@@ -77,6 +103,25 @@ connection
          res.status(404).send(error);
       })
   })
+
+
+app.post('/post', (req,res) => {
+  const newUser = req.body.user;
+  User.create({
+    name: newUser.name,
+    email: newUser.email
+  })
+     .then((user) => {
+       res.json(user);
+     })
+     .catch((error) => {
+       console.log(error);
+       res.status(404).send(error);
+     });
+})
+
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
